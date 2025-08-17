4880eb0fa237d40035b8a49b52d6b07f13e66927
Anant Mishra <anantmishra0761@gmail.com>
2025-08-17 17:58:06 +0530

    Assignment 5 completed

---
+++ Task_2.py
@@ -0,0 +1,11 @@
+list1=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print("Original List: ", list1)
+
+first_five_elements=list1[:5]
+
+reverse_first_five= first_five_elements[::-1]
+
+print("Extract first five element: ", first_five_elements)
+
+print("Reverse extracted elements: ", reverse_first_five)
